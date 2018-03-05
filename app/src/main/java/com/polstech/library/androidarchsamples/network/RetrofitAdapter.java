132da5dfdb5b3c6a1258c3a74f1001ae065b9f8d
Prashant Pol <prashantspol123@gmail.com>
2018-03-05 17:27:24 +0900

    Modify to add retrofit dependency and sample resource

---
+++ app/src/main/java/com/polstech/library/androidarchsamples/network/RetrofitAdapter.java
@@ -0,0 +1,16 @@
+package com.polstech.library.androidarchsamples.network;
+
+import javax.inject.Singleton;
+
+import retrofit2.Retrofit;
+
+/**
+ * Created by prashant.pol on 3/5/2018.
+ */
+@Singleton
+public class RetrofitAdapter {
+
+    public Retrofit getRetrofit() {
+        return null;
+    }
+}
